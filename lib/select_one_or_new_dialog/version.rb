fcc715be8e7f48e313b9af3b910fe6fd90354d3a
Bryan Larsen <bryan@larsen.st>
2012-03-29 21:37:28 -0400

    fix EDIT_LINK_BASE

--- lib/select_one_or_new_dialog/version.rb
+++ lib/select_one_or_new_dialog/version.rb
@@ -1,5 +1,5 @@
 module SelectOneOrNewDialog
   VERSION = "0.0.1"
 
-  EDIT_LINK_BASE = 'https://github.com/bryanlarsen/select_one_or_new_dialog/edit/master'
+  EDIT_LINK_BASE = 'https://github.com/Hobo/select_one_or_new_dialog/edit/master'
 end
