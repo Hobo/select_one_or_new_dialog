e3485c5ca51796a7573cd8df2428dbe65d1a76ca
Ignacio Huerta <ignacio@ihuerta.net>
2014-11-19 09:03:48 +0100

    Add missing constant EDIT_LINK_BASE

--- lib/select_one_or_new_dialog.rb
+++ lib/select_one_or_new_dialog.rb
@@ -2,6 +2,8 @@ module SelectOneOrNewDialog
 
   @@root = Pathname.new File.expand_path('../..', __FILE__)
   def self.root; @@root; end
+    
+  EDIT_LINK_BASE = "https://github.com/Hobo/select_one_or_new_dialog/edit/master"
 
   if defined?(Rails)
     require 'select_one_or_new_dialog/railtie'
